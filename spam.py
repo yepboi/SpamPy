acbae958555af47b958a8ecfbb3c00d73590cc87
yepboi <122094318+yepboi@users.noreply.github.com>
2023-09-16 18:26:06 +0600

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,14 @@
+import pyautogui,time
+time.sleep(1380)
+for i in range(25):
+    time.sleep(1)
+    pyautogui.write("Spamming")
+    pyautogui.press("enter")
+
+
+# while True:           --------for unlimited
+# for i in range(10):   --------for count a number of spam 
+# time.sleep(5)         --------for set a time
+
+
+
